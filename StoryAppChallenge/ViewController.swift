4a9bdf47e1cb5eb8b47343f2e20818996e1ccec0
Lauren a.m <laurenalicea1@gmail.com>
2021-04-16 10:43:55 -0500

    started story app got first two views kind of working

---
+++ StoryAppChallenge/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoryAppChallenge
+//
+//  Created by A.M. Student on 4/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
